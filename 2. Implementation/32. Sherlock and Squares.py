a489a21410da52da693ea9827d0c98f6f7b08b07
Bhanuka Rathnayaka <bhanuka2017@gmail.com>
2018-04-01 21:25:15 +0530

    ***

--- 2. Implementation/32. Sherlock and Squares.py
+++ 2. Implementation/32. Sherlock and Squares.py
@@ -1,13 +1,10 @@
-import math
+from math import *
 
-t = int(input())
+t = input()
 for _ in range(t):
-    c=0
-    s, e = map(int, input().split(' '))
-    s = math.ceil(math.sqrt(s))
-    for i in range(s, e):
-        if i*i>e:
-            break
-        c+=1
-            
-    print(c)
+    a, b = map(int, input().split(' '))
+    
+    a = ceil(sqrt(a))
+    b = floor(sqrt(b))
+    
+    print int(b-a) + 1
