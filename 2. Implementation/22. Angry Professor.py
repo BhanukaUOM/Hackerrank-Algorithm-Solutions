fa10a53d64d43b519b6550728acacb1fe2e768c5
Bhanuka Rathnayaka <bhanuka2017@gmail.com>
2018-04-01 17:18:13 +0530

    ***

--- 2. Implementation/22. Angry Professor.py
+++ 2. Implementation/22. Angry Professor.py
@@ -2,9 +2,6 @@ t = int(input())
 for _ in range(t):
     n, k = map(int, input().split(' '))
     arr = list(map(int, input().split(' ')))
+    arr.sort()
     
-    c = 0
-    for i in arr:
-        if i<=0:
-            c+=1
-    print("YES" if c<k else "NO")
+    print("YES" if arr[k-1]>0 else "NO")
