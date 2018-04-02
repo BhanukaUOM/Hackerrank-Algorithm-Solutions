fe5154b6ccb3442f4b46043fe62a40978af2eca8
Bhanuka Rathnayaka <bhanuka2017@gmail.com>
2018-04-02 19:24:05 +0530

    Update 41. ACM ICPC Team.py

--- 2. Implementation/41. ACM ICPC Team.py
+++ 2. Implementation/41. ACM ICPC Team.py
@@ -1,1 +1,20 @@
+n, m = map(int, input().split(' '))
+arr = []
 
+max = 0
+count = 0
+
+for _ in range(n):
+    arr.append(int(input(), 2))
+
+for i in range(n):
+    for j in range(i+1, n):
+        if (bin(arr[i] | arr[j])).count('1') == max:
+            count += 1
+            
+        elif (bin(arr[i] | arr[j])).count('1') > max:
+            max = (bin(arr[i] | arr[j])).count('1')
+            count = 1
+    
+print (max)
+print (count)
