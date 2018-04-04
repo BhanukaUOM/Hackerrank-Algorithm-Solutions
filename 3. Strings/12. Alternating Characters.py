e87102e0c35b52ce0e612447c704e1c9c79fc984
Bhanuka Rathnayaka <bhanuka2017@gmail.com>
2018-04-04 22:05:22 +0530

    ****
    If there are  n consecutive same characters, deleting n-1 of those n characters and repeating this process till we reach the end of the string, will result in a string where two consecutive characters are different.

--- 3. Strings/12. Alternating Characters.py
+++ 3. Strings/12. Alternating Characters.py
@@ -4,13 +4,7 @@ for _ in range(t):
     s = input()
     
     c = 0
-    i = 0
-    j = 1
-    while j < len(s):
-        if abs(ord(s[i]) - ord(s[j])) != 1:
-            j += 1
+    for i in range(len(s)-1):
+        if abs(ord(s[i]) - ord(s[i+1])) != 1:
             c += 1
-        else:
-            i = j
-            j += 1
     print (c)
