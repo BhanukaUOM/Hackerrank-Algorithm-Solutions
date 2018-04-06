1cde2104e4611ca8702664ce9440d4248a442908
Bhanuka Rathnayaka <bhanuka2017@gmail.com>
2018-04-06 13:15:48 +0530

    ***

--- 3. Strings/17. Anagram.py
+++ 3. Strings/17. Anagram.py
@@ -1,3 +1,5 @@
+from collections import Counter as c
+
 t = int(input())
 
 for _ in range(t):
@@ -6,19 +8,5 @@ for _ in range(t):
     
     if n%2!=0:
         print(-1)
-    else:
-        c = 0
-        c1 = [0 for i in range(26)]
-        c2 = [0 for i in range(26)]
-
-        for i in s[:n//2]:
-            c1[ord(i)-ord('a')] += 1
-         
-        for i in s[n//2:]:
-            c2[ord(i)-ord('a')] += 1
-        
-        for i in range(26):
-            if c1[i]>c2[i]:
-                c += c1[i]-c2[i]
-                
-        print(c)
+    else:                
+        print(sum((c(s[:n//2])-c(s[n//2:])).values()))
