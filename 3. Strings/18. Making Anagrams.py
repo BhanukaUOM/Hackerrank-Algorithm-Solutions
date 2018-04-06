6b984e5de3a164dc70c2ace25957b9b73e44d81f
Bhanuka Rathnayaka <bhanuka2017@gmail.com>
2018-04-06 13:41:28 +0530

    ***

--- 3. Strings/18. Making Anagrams.py
+++ 3. Strings/18. Making Anagrams.py
@@ -12,9 +12,6 @@ for i in s2:
     c2[ord(i)-ord('a')] += 1
 
 for i in range(26):
-    if c1[i]>0 and c2[i]>0:
-        while min(c1[i],c2[i])>0:
-            c1[i] -= 1
-            c2[i] -= 1
+    c += abs(c1[i]-c2[i])
 
-print(sum(c2)+sum(c1))
+print(c)
