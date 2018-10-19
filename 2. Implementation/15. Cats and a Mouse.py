2f49c22e4049eaaecfaa53c9d6f983cc9ac0ab62
mkraghav <mkumar199861@gmail.com>
2018-10-19 12:05:20 +0530

    options together and the conditionals together
    did the same except I always like to put the options together and the conditionals together for cleanliness

--- 2. Implementation/15. Cats and a Mouse.py
+++ 2. Implementation/15. Cats and a Mouse.py
@@ -2,4 +2,4 @@ t = int(input())
 
 for _ in range(t):
     x, y, z = map(int, input().split(' '))
-    print("Cat A" if abs(z - x) < abs(z - y) else "Cat B" if abs(z - x) > abs(z - y) else "Mouse C")
+    print(['Cat A','Cat B', 'Mouse C'][0 if abs(x-z) < abs(y-z) else 1 if abs(x-z) > abs(y-z) else 2])
