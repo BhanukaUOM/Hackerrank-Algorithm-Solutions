f5d0db66b29f7967537d6044ee620aa638ef5285
mkraghav <mkumar199861@gmail.com>
2018-10-19 11:55:35 +0530

    simple way to implement

--- 2. Implementation/06. Birthday Chocolate.py
+++ 2. Implementation/06. Birthday Chocolate.py
@@ -3,13 +3,10 @@ s = [int(x) for x in input().split()]
 assert n == len(s)
 d, m = map(int, input().split())
 
-r = 0
-if n >= m:
-    a = sum(s[:m])
-    if a == d:
-        r += 1
-    for j in range(m, n):
-        a += s[j] - s[j-m]
-        if a == d:
-            r += 1
-print(r)
+count = 0
+for i in range(0,n):
+    total = sum(s[i:m+i])
+    if total == d:
+        count+=1
+ 
+print(count)
